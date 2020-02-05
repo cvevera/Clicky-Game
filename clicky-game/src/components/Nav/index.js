9912e97e9512d8364dc288190ea9eafa2a95d40f
cvevera <cvevera@gmail.com>
2020-02-05 14:11:11 -0500

    css updated

--- clicky-game/src/components/Nav/index.js
+++ clicky-game/src/components/Nav/index.js
@@ -2,28 +2,19 @@ import React from "react";
 import "./style.css";
 
 function Nav(props) {
-  return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a classNameName="navbar-brand" href="#">Navbar w/ text</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarText">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Features</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Pricing</a>
-      </li>
-    </ul>
-    <span className="navbar-text">
-      Navbar text with an inline element
-    </span>
+  return <div class="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">Star Wars Memory Game</h1>
+    <p className="lead">Click all the images to win. Don't pick the same one twice!</p>
+    <div className ="container">
+    <div className ="row">
+        <div className="col-6"><h3>High Score</h3></div>
+        <div className="col-6"><h3>Your Score</h3></div>
+    </div>
+
+    </div>
   </div>
-</nav>
+</div>
 }
 
 export default Nav;
