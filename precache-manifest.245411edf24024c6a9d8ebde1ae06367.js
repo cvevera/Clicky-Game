af42a41797955bb00d7b762868485d51ad464e5b
cvevera <cvevera@gmail.com>
2020-02-06 22:07:09 -0500

    Updates

---
+++ precache-manifest.245411edf24024c6a9d8ebde1ae06367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d493d9364fcc9ff630783b8dad1b1827",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "cee847f87a2c6f92e2b1",
+    "url": "/Clicky-Game/static/css/main.f64d41d2.chunk.css"
+  },
+  {
+    "revision": "8df644a2450638befb0c",
+    "url": "/Clicky-Game/static/js/2.f3af9676.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky-Game/static/js/2.f3af9676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee847f87a2c6f92e2b1",
+    "url": "/Clicky-Game/static/js/main.8da39ab0.chunk.js"
+  },
+  {
+    "revision": "ed33e96b23e5ad0b910f",
+    "url": "/Clicky-Game/static/js/runtime-main.da6617cf.js"
+  },
+  {
+    "revision": "df2d94e1c627019a31645b60638dec19",
+    "url": "/Clicky-Game/static/media/blue-and-purple-cosmic-sky-956999.df2d94e1.jpg"
+  }
+]);
